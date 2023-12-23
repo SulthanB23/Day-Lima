def912a8d259d4eeaf718aacddf56b6731498100
SulthanB23 <sulthanbonang23@gmail.com>
2023-12-23 16:49:56 +0700

    challange selesai

--- Challange-Variabel.js
+++ Challange-Variabel.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+console.log("Hello semua");
+
+// challange variable & pemanipulasi string
+// buat file baru di repo yang sama, nama file "challange-variable"
+// tampilkan data menggunakan backtick / ``
+
+const namaLengkap = "Sulthan Bonang Hendrajat"
+const namaPanggilan = "Sulthan"
+
+let result = `Perkenalkan nama saya ${namaLengkap}, biasa dipanggil ${namaPanggilan}`
+
+console.log(result);
+// ganti saya jadi aku (string manipulative)
+console.log(result.replaceAll(`saya`, `aku`));
